da2c64ea1a589cce6ea1ee3b2bd2fc3c7b0940f8
Cloud User <ec2-user@ip-172-31-31-231.ec2.internal>
2024-05-25 00:08:47 +0000

    jaa

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello():
-    return 'Hello, Python!'
+    return 'I and Sonam are senior Devops engineers with 400K package so please stop and fuckk off!!!'
 
 # Run the Flask application
 if __name__ == '__main__':
